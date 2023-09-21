855f6744317e476ec71f7c99b560bacabf902861
jiayinldf <jiayinly@126.com>
2023-09-21 17:07:07 +1200

    添加项目文件。

---
+++ 第一章.cpp
@@ -0,0 +1,62 @@
+//例1.1
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "This is a C++ program";
+	return 0;
+}
+
+//例1.2 求a和b两个数的和
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, sum;
+	cin >> a >> b;
+	sum = a + b;
+	cout << "a+b=" << sum<<endl;
+	return 0;
+}
+
+
+
+//例1.3 给两个数x和y，求两数中的大者。
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int max(int x, int y);
+	int a, b, z;
+	cout << "please enter two number:"<<endl;
+	cin >> a >> b;
+	z = max(a, b);
+	cout <<"The max number is:" <<endl<< z << endl;
+	return 0;
+
+}
+
+int max(int x, int y)
+{
+	int z;
+	if (x>y)
+	{
+		z = x;
+	}
+	else if (x<y)
+	{
+		z = y;
+	}
+	else
+	{
+		z = x;
+	}
+	return z;
+	
+}
+
+*/
+
+//例题：1.4 包含类的C++程序
