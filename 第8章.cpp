f5badc5f667e38c528bfe07eb9ae611973baf382
jiayinldf <jiayinly@126.com>
2023-09-22 11:01:21 +1200

    11

---
+++ 第8章.cpp
@@ -0,0 +1,27 @@
+//声明一个类，并定义两个类对象
+/*
+#include<iostream>
+using namespace std;
+
+class Student
+{
+public:
+	void display()
+	{
+		cout << "num:" << num << endl;
+		cout << "name:" << name << endl;
+		cout << "sex:"<< sex << endl;
+
+	}
+
+private:
+	int num;
+	char name[20];
+	char sex;
+
+};
+
+Student stdu1, stdu2;
+
+*/
+
