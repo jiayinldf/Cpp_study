6ee2d9ca35e6e17c916a78e0d1824067a0c4aa95
jiayinldf <jiayinly@126.com>
2023-09-25 11:25:34 +1300

    2023-9-25

--- 第8章.cpp
+++ 第8章.cpp
@@ -23,5 +23,60 @@ private:
 
 Student stdu1, stdu2;
 
-*/
 
+
+//8.4.2
+#include<iostream>
+using namespace std;
+class Time
+{
+public:
+	int hopur;
+	int minute;
+};
+Time t, * p;
+p = &t;
+cout << p->hour;
+
+
+
+//8.4.3
+Time t1;
+Time &t2=t1;
+cout<<t2.hour;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//8.6 类和对象的简单应用举例
+
+//例8.1用类来实现输入和输出时间（时：分：秒：）
+#include<iostream>
+using namespace std;
+class Time
+{
+public:
+	int hour;
+	int minute;
+	int sec;	
+};
+
+int main()
+{
+	Time t1;
+	cin >> t1.hour;
+	cin >> t1.minute;
+	cin >> t1.sec;
+	cout << t1.hour << ":" << t1.minute << ":" << t1.sec << endl;
+	return 0;
+}
+*/
